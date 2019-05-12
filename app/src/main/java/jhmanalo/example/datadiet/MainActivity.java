1329991a466be7d2f3ff39a48f55c28e666be747
Jeremy Manalo <jeremymanalo@Jeremys-Macbook-Pro.attlocal.net>
2019-05-11 20:02:29 -0700

    UI changes from Jeremy again

--- app/src/main/java/jhmanalo/example/datadiet/MainActivity.java
+++ app/src/main/java/jhmanalo/example/datadiet/MainActivity.java
@@ -70,12 +70,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         FirebaseApp.initializeApp(this);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-        {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        }
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
